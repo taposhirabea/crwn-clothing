318bd482fa318eb87cb788aa9b3780d91e2ef735
unknown <taposhirabea3@gmail.com>
2019-11-09 00:34:36 +0600

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './components/pages/homepage/homepage.component';
 
 function App() {
-  return <div >
+  return ( 
+    <div >
       <HomePage />
-    </div>;
+    </div>
+  );
   
 }
 
