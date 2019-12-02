54fd13757373a09f3dd819249007a7125f63b949
unknown <taposhirabea3@gmail.com>
2019-12-02 16:29:55 +0600

    removing redux-logger from production build

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -9,15 +9,15 @@ import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
-    <div className='directory-menu'>
-        {sections.map(({id, ...otherSectionProps }) => (
-            <MenuItem key={id} {...otherSectionProps} />
-        ))}
-    </div>
+  <div className='directory-menu'>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-    sections: selectDirectorySections
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
