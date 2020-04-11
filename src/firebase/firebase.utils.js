fe73991738b86c9f18aa8c52beb10ddd322c3b3d
unknown <taposhirabea3@gmail.com>
2020-04-11 15:25:22 +0600

    implement code that pulls in our shop data from firestore and stores it into the shop reducer from our shop page. Also included our withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,7 @@ const config = {
     messagingSenderId: '227069843369',
     appId: '1:227069843369:web:20ec48db35bba5205356b8'
   };
+  firebase.initializeApp(config);
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
@@ -18,34 +19,65 @@ const config = {
     const userRef = firestore.doc(`users/${userAuth.uid}`);
 
     const snapShot = await userRef.get();
-
-    if(!snapShot.exists) {
-      const { displayName, email } = userAuth;
+  
+    if (!snapShot.exists) {
+      const {displayName,email } = userAuth;
       const createdAt = new Date();
-
       try {
         await userRef.set({
           displayName,
           email,
           createdAt,
           ...additionalData
-        })
-
+        });
       } catch (error) {
-          console.log('error creating user', error.message);
+        console.log('error creating user', error.message);
       }
-      
     }
+  
+
+
     return userRef;
   };
 
-  firebase.initializeApp(config);
+  export const addCollectionAndDocuments = async (
+    collectionKey, 
+    objectsToAdd
+    ) => {
+    const collectionRef = firestore.collection(collectionKey);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
 
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
+    return await batch.commit();
+  };
 
-export default firebase;
+  export const convertCollectionsSnapshotToMap = collectionsSnapshot => {
+    const transformedCollection = collectionsSnapshot.docs.map(docSnapshot => {
+      const { title, items } = docSnapshot.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: docSnapshot.id,
+        title,
+        items
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    } , {});
+  };
+  
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account' });
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  
+  export default firebase;
